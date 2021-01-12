1fc167fb1f77d5fe436fc3a31ccba76d828e7776
SauravMaheshkar <sauravvmaheshkar@gmail.com>
2021-01-12 14:18:11 +0530

    Update Bash

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "[general]
-email = \"email@com\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
